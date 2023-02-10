54d10350ba3ef660b92fead55672c0c8ef335cc9
vitoriabelmiro <vitoria@hbifinanceira.com.br>
2023-02-10 15:19:17 -0300

    fix: next.config caminho

--- pages/index.js
+++ pages/index.js
@@ -5,35 +5,33 @@ import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/timeline";
 
 function HomePage() {
-    const estilohomepage = { //backgroundColor: "red" 
-    };
+  const estilohomepage = {
+    //backgroundColor: "red"
+  };
 
-    //console.log(config.playlists);
+  //console.log(config.playlists);
 
-    return (
+  return (
+    <>
+      <CSSReset />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          flex: 1,
+          // backgroundColor: "red",
+        }}
+      >
+        <Menu />
+        <Header />
 
-        <>
-        <CSSReset/>
-        <div style={{
-                display: "flex",
-                flexDirection: "column",
-                flex: 1,
-                // backgroundColor: "red",
-            }}>
-            <Menu/>
-            <Header />
-            
-            <Timeline playlists={config.playlists} >
-            </Timeline>
-
-        </div>
-        
-        </>
-        
-    )
+        <Timeline playlists={config.playlists}></Timeline>
+      </div>
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
 
 /*function Menu() {
     return (
@@ -46,80 +44,59 @@ const StyledHeader = styled.div`
     width: 80px;
     height: 80px;
     border-radius: 50px; //borda redonda
-
   }
 
   .user-info {
     margin-top: 50px;
-        display: flex;
-        align-items: center;
-        width: 100%;
-        padding: 16px 32px;
-        gap: 16px;
+    display: flex;
+    align-items: center;
+    width: 100%;
+    padding: 16px 32px;
+    gap: 16px;
   }
-
-
-  `;
+`;
 
 function Header() {
-    return (
+  return (
+    <StyledHeader>
+      {/*<img src="banner"/>*/}
 
-        <StyledHeader>
-
-            {/*<img src="banner"/>*/}
-
-            <section className="user-info">
-
-                <img src={`https://github.com/${config.github}.png`} />
-                <div>
-                    <h2>
-                        {config.name}
-                    </h2>
-                    <p>
-                        {config.job}
-                    </p>
-
-                </div>
-
-            </section>
-
-        </StyledHeader>
-    )
+      <section className="user-info">
+        <img src={`https://github.com/${config.github}.png`} />
+        <div>
+          <h2>{config.name}</h2>
+          <p>{config.job}</p>
+        </div>
+      </section>
+    </StyledHeader>
+  );
 }
 
 function Timeline(propriedade) {
-    console.log("dentro do componente", propriedade.playlists);
-    const playlistsNames = Object.keys(propriedade.playlists);
-    return (
-        <StyledTimeline>
-            {playlistsNames.map(function (playlistsNames) { //map é um tipo de for
+  console.log("dentro do componente", propriedade.playlists);
+  const playlistsNames = Object.keys(propriedade.playlists);
+  return (
+    <StyledTimeline>
+      {playlistsNames.map(function (playlistsNames) {
+        //map é um tipo de for
 
-                const videos = propriedade.playlists[playlistsNames];
+        const videos = propriedade.playlists[playlistsNames];
+        return (
+          <section>
+            <h2>{playlistsNames}</h2>
+            <div>
+              {videos.map((video) => {
                 return (
-                    <section>
-                        <h2>{playlistsNames}</h2>
-                        <div>
-                            {
-                                videos.map((video) => {
-
-                                    return (
-                
-                                        <a href={video.url}>
-                                            <img src={video.thumb} />
-                                            <span>
-                                                {video.title}
-                                            </span>
-                                        </a>
-                
-                                    )
-                                })
-                            }
-                        </div>
-                    </section>
-                )
-
-            })}
-
-</StyledTimeline>
-    )
+                  <a href={video.url}>
+                    <img src={video.thumb} />
+                    <span>{video.title}</span>
+                  </a>
+                );
+              })}
+            </div>
+          </section>
+        );
+      })}
+    </StyledTimeline>
+  );
 }
